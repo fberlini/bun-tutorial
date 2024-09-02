f607f8b06f2eee1f65047e2a326170db05e6609c
Felipe Berlini <felipe.berlini@gmail.com>
2024-09-02 12:07:02 +0200

    routes and erros

--- index.ts
+++ index.ts
@@ -1,7 +1,29 @@
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Hello World! Are you learning Bun, dude?")
+        const url = new URL(req.url)
+
+        if (url.pathname === '/') {
+            return new Response("Hello World! Are you learning Bun, dude?")    
+        }
+        
+        if (url.pathname === '/about') {
+            return new Response("About me")    
+        }
+
+        // Handle error
+        if (url.pathname === '/feed') {
+            throw new Error('Could not fetch')
+        }
+
+        return new Response('404!')
+    },
+    error(error) {
+        return new Response(`<pre> ${error} \n ${error.stack} </pre>`, {
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        })
     }
 })
 
